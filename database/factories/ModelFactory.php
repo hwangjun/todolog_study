75281d3ebb70f01add497054f96defeb754a907c
hwangjun <hwangjunses@naver.com>
2016-08-07 15:24:11 +0900

    모델팩토리 수정

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,10 +12,39 @@
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
+   /* return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];*/
+
+    // 집계 함수를 이용해 id의 최솟값과 최댓값을 가져옴
+
+    $min = App\User::min('id');
+    $max = App\User::max('id');
+
+    return [
+        'user_id' => $faker->numberBetween($min,$max),
+        'name' => substr($faker->word, 0, 20),
+        'description' => $faker->sentence,
+        'created_at' => $faker->dateTimeBetween($startDate = '-2 years', $endDate = '-1 years'),
+        'update_at' => $faker->dateTimeBetween($startDate = '-1 yers', $endDate = 'now'),
+    ];
+});
+
+$factory->define(App\Task::class, function (Faker\Generator $faker){
+    $min = App\Project::mind('id');
+    $max = App\Project::max('id');
+
+    $dt = $faker->dateTimeBetween($startDate = '-1 months', $endDate= 'now');
+
+    return [
+        'project_id' => $faker->numberBetween($min,$max),
+        'name' => substr($faker->sentence,0,20),
+        'description' => $faker->text,
+        'due_date' => $faker->dateTimeBetween($startDate = '-1 months', $endDate = '+1 months'),
+        'created_at' => $dt,
+        'update_at' => $dt,
     ];
 });
